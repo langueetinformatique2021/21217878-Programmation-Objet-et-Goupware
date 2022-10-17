ebfdf9ad94be2140d1e51cb1d6965015aac26349
semmiehu <semmiehu@localhost>
2022-10-17 06:46:03 +0200

    21217878_Hu YUWEN

---
+++ yuwen_21217878/src/yuwen_21217878/ex_5.java
@@ -0,0 +1,17 @@
+package yuwen_21217878;
+import java.util.Arrays;
+public class ex_5 {
+
+public static void main(String[] args) {
+	
+	int p1 = 10, p2 = 9, p3 = -1, p4 = 4, p5 = 8;
+	int p6;
+	int[] num = {p1,p2,p3,p4,p5};
+	Arrays.sort(num);
+	p6 = num[2];
+	System.out.println(p6);
+}
+	
+	
+
+}
