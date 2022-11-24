56fc83f68c531a2769810879776e3fdf71e82519
semmiehu <semmiehu@semmiedeair>
2022-11-24 11:00:36 +0100

    lance des

--- yuwen_21217878/src/td8/LanceDes.java
+++ yuwen_21217878/src/td8/LanceDes.java
@@ -3,9 +3,9 @@ package td8;
 public class LanceDes {
 
 	public static void main(String[] args) {
-		double d = Math.random();// 生成[0，1）的随机数
-		int a = (int) (Math.random() * 6) + 1;// 因为骰子的点数是[1,6]，所以要加1，将doubl类型的数据强制转换为int类型数据。
-		int b = (int) (Math.random() * 6) + 1;
+		double d = Math.random();// create random number from 0-1
+		int a = (int) (Math.random() * 6) + 1;// cause the dice face has 1-6, so i add 1,make double b int因为骰子的点数是[1,6]，
+		int b = (int) (Math.random() * 6) + 1;//因为骰子的点数是[1,6]，所以要加1，将double类型的数据强制转换为int类型数据。
 		int c = (int) (Math.random() * 6) + 1;
 		int count = a + b + c;
 		if (count > 15) {
