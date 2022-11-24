32ca548a841ebb2e0ff4237a0cf8252a2be3e0ce
semmiehu <semmiehu@semmiedeair>
2022-11-24 09:51:01 +0100

    division cours8

---
+++ yuwen_21217878/src/devoir5/Division.java
@@ -0,0 +1,25 @@
+package devoir5;
+
+public class Division {
+	
+	public static void main(String[] args) {
+		try
+		{
+		int i = 1000, j;
+		do {i--; j = 1 / i;} while (true);
+		
+		   // 程序代码
+		}catch(Exception n1) {
+		
+		   //Catch 块
+		
+	}
+	}
+}
+
+/**ava 中的异常（Exception）又称为例外，是一个在程序执行期间发生的事件，
+ * 它中断正在执行程序的正常指令流。 
+ * 为了能够及时有效地处理程序中的运行错误，必须使用异常类。
+ * 
+ * 
+ */
